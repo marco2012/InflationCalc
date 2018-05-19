27843e4f76b2e6790087c691c1b7cc9adcbf6d38
marco2012 <marco.fox.marco@gmail.com>
2018-05-19 19:00:57 +0200

    Initial Commit

---
+++ Inflation Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Inflation Calculator
+//
+//  Created by Marco on 19/05/2018.
+//  Copyright © 2018 Vikings. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
